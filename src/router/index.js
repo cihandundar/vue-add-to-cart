fdd15dc3bd2524a0b5c077522a211be1f73616d4
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-12-21 21:42:00 +0300

    router page created

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
-
+import Product from "../views/Product.vue";
+import Cart from "../views/Cart.vue";
+import ProductDetail from "../views/ProductDetail.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -13,6 +15,11 @@ const router = createRouter({
       path: "/product",
       name: "product",
       component: Product,
+    },
+    {
+      path: "/product/:id",
+      name: "productDetail",
+      component: ProductDetail,
     },
     {
       path: "/cart",
