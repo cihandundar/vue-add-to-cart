2e470b3af1812168db12e9dce72cc4ec7690bda7
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-12-21 21:41:49 +0300

    navbar page created

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,15 @@
 <template>
   <header>
-    <div class="wrapper">
+    <div class="header-container">
+      <div class="logo">
+        <h1>Logo</h1>
+      </div>
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <ul>
+          <li><RouterLink to="/">Home</RouterLink></li>
+          <li><RouterLink to="/product">Product</RouterLink></li>
+          <li><RouterLink to="/cart">Cart</RouterLink></li>
+        </ul>
       </nav>
     </div>
   </header>
@@ -11,6 +17,34 @@
   <RouterView />
 </template>
 
-<script setup></script>
+<script></script>
 
-<style></style>
+<style scoped>
+.header-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 20px 0;
+}
+
+.header-container > .logo h1 {
+  font-size: 3rem;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+}
+
+nav ul {
+  display: flex;
+  align-items: center;
+}
+
+nav ul li:not(:last-child) {
+  margin-right: 30px;
+}
+
+nav ul li a:hover {
+  color: rgb(88, 86, 86);
+}
+</style>
