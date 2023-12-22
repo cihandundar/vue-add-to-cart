85d8e029671a1f786a46bf49b7beeffda4f9d376
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-12-22 22:46:47 +0300

    fix

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,7 +1,22 @@
 <template>
-  <div>Cart</div>
+  <div class="cart-container">
+    <h2>Cart</h2>
+    <ul>
+      <li v-for="(item, index) in cartItems" :key="index">
+        {{ item.title }} - Quantity: {{ item.quantity }}
+      </li>
+    </ul>
+  </div>
 </template>
 
-<script></script>
+<script setup>
+import { ref, onMounted } from "vue";
+import { useRoute } from "vue-router";
 
-<style></style>
+const route = useRoute();
+const cartItems = ref([]);
+
+onMounted(() => {
+  cartItems.value = route.params.cartItems || [];
+});
+</script>
