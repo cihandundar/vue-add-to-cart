289cbd383c295f249f975af3d62355c8d900c78c
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-12-23 16:58:55 +0300

    fix

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,22 +1,113 @@
 <template>
-  <div class="cart-container">
-    <h2>Cart</h2>
-    <ul>
-      <li v-for="(item, index) in cartItems" :key="index">
-        {{ item.title }} - Quantity: {{ item.quantity }}
-      </li>
-    </ul>
-  </div>
+  <section class="cart-section">
+    <div class="cart-container">
+      <div class="cart-container-title">
+        <h1>Shopping Cart</h1>
+        <div v-if="cartItems.length === 0" class="empty-cart-message">
+          <img :src="shoppingUrl" />
+          <p>Your cart is empty</p>
+          <button><RouterLink to="/product">Shopping Now</RouterLink></button>
+        </div>
+      </div>
+      <ul class="cart-items">
+        <li v-for="item in cartItems" :key="item.id">
+          <div class="cart-item">
+            <div class="cart-item-image">
+              <img :src="item.image" alt="Product Image" />
+            </div>
+            <div class="cart-item-price">
+              <h3>{{ item.title }}</h3>
+              <p>{{ item.price }} $</p>
+              <p>Quantity: {{ item.quantity }}</p>
+              <button @click="removeItem(item.id)">Remove</button>
+            </div>
+          </div>
+        </li>
+      </ul>
+      <div v-if="cartItems.length > 0" class="total">
+        <h3>Total: {{ cartTotal }} $</h3>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
-import { useRoute } from "vue-router";
+import { useStore } from "vuex";
+import shoppingUrl from "../assets/images/shopping.jpg";
+const store = useStore();
+const cartItems = store.state.cart;
+const cartTotal = store.getters.cartTotal;
 
-const route = useRoute();
-const cartItems = ref([]);
-
-onMounted(() => {
-  cartItems.value = route.params.cartItems || [];
-});
+const removeItem = (productId) => {
+  store.dispatch("removeFromCart", productId);
+};
 </script>
+
+<style scoped>
+.cart-container {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+
+.cart-container-title {
+  text-align: center;
+}
+
+.empty-cart-message {
+  margin-top: 30px;
+}
+
+.empty-cart-message img {
+  width: 300px;
+}
+.empty-cart-message button {
+  margin-top: 10px;
+  padding: 15px 40px;
+  background-color: rgb(63, 0, 63);
+  border: none;
+  cursor: pointer;
+}
+
+.empty-cart-message button a {
+  color: #fff;
+}
+
+.cart-items {
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+}
+.cart-item {
+  width: 280px;
+  margin: 10px;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
+    rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+  padding: 30px;
+  text-align: center;
+  border-radius: 10px;
+}
+.cart-item-image img {
+  width: 100%;
+  height: 250px;
+  object-fit: contain;
+}
+
+.cart-item-price button {
+  margin-top: 10px;
+  padding: 15px 40px;
+  background-color: rgb(63, 0, 63);
+  border: none;
+  cursor: pointer;
+  color: #fff;
+  border-radius: 8px;
+}
+
+.cart-item-price h3 {
+  text-transform: uppercase;
+  margin-block: 0.62rem;
+  display: -webkit-box;
+  -webkit-line-clamp: 1;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+}
+</style>
