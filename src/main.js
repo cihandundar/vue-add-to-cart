1925795cc6a73346dc51fe8913b378b58db75991
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-12-23 16:58:32 +0300

    toastify imported

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import "./assets/style.css";
 import { createApp } from "vue";
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store/store";
@@ -7,4 +9,5 @@ import store from "./store/store";
 const app = createApp(App);
 app.use(store);
 app.use(router);
+app.use(Toast);
 app.mount("#app");
