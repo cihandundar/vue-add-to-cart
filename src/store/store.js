8dca97020d0e1e5ab126a547962780b3e6acd5bc
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-12-23 16:58:49 +0300

    local storage and toastify added

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,15 @@
 import { createStore } from "vuex";
 import axios from "axios";
+import { useToast } from "vue-toastification";
+
+const toast = useToast();
 
 const store = createStore({
   state() {
     return {
       cart: [],
       products: [],
+      cartTotal: 0,
     };
   },
   mutations: {
@@ -20,7 +24,30 @@ const store = createStore({
       } else {
         state.cart.push({ ...product, quantity: 1 });
       }
-      console.log("addtocart", product);
+      toast.success("Added to cart 🚀");
+      localStorage.setItem("cart", JSON.stringify(state.cart));
+    },
+    removeFromCart(state, productId) {
+      const index = state.cart.findIndex((item) => item.id === productId);
+      if (index !== -1) {
+        state.cart.splice(index, 1);
+        toast.error("Removed from cart 🗑️");
+        localStorage.setItem("cart", JSON.stringify(state.cart));
+      }
+    },
+    updateCartTotal(state, updatedTotal) {
+      state.cartTotal = updatedTotal;
+      localStorage.setItem("cartTotal", updatedTotal);
+    },
+    loadCartFromLocalStorage(state) {
+      const cart = localStorage.getItem("cart");
+      const cartTotal = localStorage.getItem("cartTotal");
+      if (cart) {
+        state.cart = JSON.parse(cart);
+      }
+      if (cartTotal) {
+        state.cartTotal = cartTotal;
+      }
     },
   },
   actions: {
@@ -35,6 +62,11 @@ const store = createStore({
     addToCart(context, product) {
       context.commit("addToCart", product);
     },
+    removeFromCart({ commit }, productId) {
+      commit("removeFromCart", productId);
+      const updatedCartTotal = context.getters.cartTotal;
+      commit("updateCartTotal", updatedCartTotal);
+    },
   },
   getters: {
     getProductById: (state) => (productId) => {
@@ -46,7 +78,16 @@ const store = createStore({
     cartItems(state) {
       return state.cart;
     },
+    cartTotal: (state) => {
+      return state.cart
+        .reduce((total, item) => {
+          return total + item.price * item.quantity;
+        }, 0)
+        .toFixed(2);
+    },
   },
 });
+
+store.commit("loadCartFromLocalStorage");
 
 export default store;
